9d8f3ba36b321cff83c53001075fd0c971d46c6f
TheCourierLE <thecourierle@gmail.com>
2025-07-24 14:25:22 +0300

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,34 @@
+
+//aritmetic operators: (+ - * /) 
+//ex. 10 = x + 4
+
+let student = 30;
+
+//student = student + 4;
+//student = student - 27;
+//student = student * 5;
+//student = student / 3;
+//student = student ** 4;
+//let extraStudents = student % 3;
+
+//augmented assignment operators: (+= -= *= /=)
+
+//student += 1;
+//student -= 1;
+//student *= 2;
+//student /= 2;
+//student **= 3;
+//student %= 3; 
+
+//student ++;
+//student --;
+
+
+/*operators precedence
+    1.parenthesis ()
+    2.exponents **
+    3.multiplication & division & modulo * / 
+    4.addition & subtraction + -
+*/
+
+console.log(student);
---
+++ variable.js
@@ -0,0 +1,36 @@
+// Numbers
+// let age = 26;
+// let price = 24.99;
+// let high = 214;
+
+// console.log(`I'am ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`My high is ${high}cm`);
+
+// String
+// let firstName = "Eugene";
+// let favoriteFood = "Burger";
+// let email = "thecourierle@gmail.com"
+
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You like ${favoriteFood}`);
+// console.log(`Your email is ${email}`);  
+
+// Booleans
+// let online = false;
+// let forSale = true;
+// let isStudent = true;
+
+// console.log(`TheCourierLE is ${online}`);
+// console.log(`IS this car for sale ${forSale}`);
+// console.log(`Enrolled ${isStudent}`);
+
+let fullName = "TheCourierLE";
+let age = 26;
+let isStudent = false;
+
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
